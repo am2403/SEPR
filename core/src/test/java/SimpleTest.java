21b5283697472caa31b8f4524948b5bd5b89afe1
Adam Hawley <adamhawley99@gmail.com>
2018-12-12 12:21:48 +0000

    Regaining Testing Setup
    Setting the testing module back to its furthest state in the previous repo.

--- core/src/test/java/SimpleTest.java
+++ core/src/test/java/SimpleTest.java
@@ -1,3 +1,7 @@
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.sepr.game.Main;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
@@ -5,5 +9,13 @@ public class SimpleTest {
     @Test
     public void test1() {
         System.out.println("Test 1 Works");
+    }
+
+    @Test
+    public void testSetup() {
+
+
+
+
     }
 }
