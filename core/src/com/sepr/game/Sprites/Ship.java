f8f6ad5e29ad2dab37afec055dbea6031481c798
aw1516 <aj.williams.up@gmail.com>
2019-01-21 04:38:06 +0000

    Revert "Revert "Merge remote-tracking branch 'origin/master'""
    This reverts commit ebbf9b15103bbd7b3c5d91fb609983726a5aba3a.

--- core/src/com/sepr/game/Sprites/Ship.java
+++ core/src/com/sepr/game/Sprites/Ship.java
@@ -99,6 +99,10 @@ public class Ship extends Sprite {
         shootTimer = 0;
 
         cannonBalls = new ArrayList<CannonBall>();
+
+        //Listens for Box2D Object collisions
+        cl = new WorldContactListener(screen);
+        world.setContactListener(cl);
     }
 
 
@@ -128,7 +132,7 @@ public class Ship extends Sprite {
         //if the cannonBall goes past the world width it gets removed from the game
         ArrayList<CannonBall> cannonBallsToRemove = new ArrayList<CannonBall>();
         for (CannonBall cannonBall : cannonBalls) {
-            cannonBall.update(dt);
+            cannonBall.update(dt, shipBody.getAngle());
             if (cannonBall.cannonBallBody.getWorldCenter().x > Gdx.graphics.getWidth() || cannonBall.cannonBallBody.getWorldCenter().x < 0) {
                 cannonBallsToRemove.add(cannonBall);
             }
@@ -198,12 +202,12 @@ public class Ship extends Sprite {
         if(shootTimer >= SHOOT_WAIT_TIME){
             shootTimer = 0; //resets the shoot timer
 
-            cannonBalls.add(new CannonBall(playScreen, 69, 77, shipBody.getAngle()));
+            cannonBalls.add(new CannonBall(combatScreen, cannon.cannonBody.getWorldCenter().x, cannon.cannonBody.getWorldCenter().y, cannon.cannonBody.getAngle()));
 
 
             //since a force is applied to the ship when we shoot our bullet, we apply an equal force in the
             //opposite direction, stopping the ship from continuously moving backwards (acting a bit like recoil)
-            shipBody.applyLinearImpulse(new Vector2(cos(cannon.cannonBody.getAngle()), sin(cannon.cannonBody.getAngle())), shipBody.getWorldCenter(), true);
+            //shipBody.applyLinearImpulse(new Vector2(cos(cannon.cannonBody.getAngle()), sin(cannon.cannonBody.getAngle())), shipBody.getWorldCenter(), true);
         }
     }
 
